9b2370962e01bb7a7963bee1b1286cd20f09a0e8
unknown <tdasilva@gamingcampus.local>
2021-11-18 12:37:50 +0100

    Changement de poste séance 18/11

--- js/script.js
+++ js/script.js
@@ -9,19 +9,23 @@ $(document).ready(function () {
   $('.parallax').parallax();
 });
 
-var count = 0;
-const audio = new Audio("sound/rickroll.mp3" );
+var count_btn_click = 0;
+var count_rick_rgb = 0;
+const audio = new Audio("sound/rickroll.mp3");
 
-function t() {
-  count = count + 1;
-  if($("#project").is(':visible')){
-    $("#project").css('display','none');
+function modal_contact() {
+  count_btn_click = count_btn_click + 1;
+  if ($("#project").is(':visible')) {
+    $("#project").css('display', 'none');
   } else {
-    $("#project").css('display','block');
+    $("#project").css('display', 'block');
   }
-  if(count == 5){
-    
-    $("body").css('animation','3s rotation_loop linear infinite');
+  rick();
+}
+function rick() {
+  if (count_btn_click == 5) {
+    $(".main").css('animation', '3s rotation_loop linear infinite');
+    $(".rick").css('animation', '3s rick_loop linear infinite');
     audio.play();
   }
 }
