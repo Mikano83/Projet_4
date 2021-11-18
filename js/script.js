c5d323929c3c53a14123297f66688269c4083d11
unknown <tdasilva@gamingcampus.local>
2021-11-18 21:09:13 +0100

    Fin de session 18/11 | INDEX

--- js/script.js
+++ js/script.js
@@ -8,12 +8,19 @@ $(document).ready(function () {
   $('.modal').modal();
   $('.parallax').parallax();
 });
+var passphrase = "";
+document.addEventListener("keydown", function(event) {
+  passphrase = passphrase + event.key;
+  console.log(passphrase);
+  password();
+})
 
 var count_btn_click = 0;
 var count_rick_rgb = 0;
-const audio = new Audio("sound/rickroll.mp3");
+const audio_rick = new Audio("sound/rickroll.mp3");
+const audio_heaven = new Audio("sound/heaven.mp3");
 
-function modal_contact() {
+function projects() {
   count_btn_click = count_btn_click + 1;
   if ($("#project").is(':visible')) {
     $("#project").css('display', 'none');
@@ -24,8 +31,19 @@ function modal_contact() {
 }
 function rick() {
   if (count_btn_click == 5) {
-    $(".main").css('animation', '3s rotation_loop linear infinite');
+    $("body").css('animation', '3s rotation_loop linear infinite');
     $(".rick").css('animation', '3s rick_loop linear infinite');
-    audio.play();
+    $(".rick").css('display', 'block');
+    audio_rick.play();
+  }
+}
+function password() {
+  if (passphrase == "antoine"){
+    $(".rick").css('display', 'block');
+    $(".rick").css('background-color', '#000000a9');
+    $(".antoine").css('display', 'block');
+    $(".antoine").css('animation', '6s heaven linear infinite');
+    $(".antoine").css('animation-direction', 'alternate');
+    audio_heaven.play();
   }
 }
